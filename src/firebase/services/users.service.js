f8ea9b692ea3dc9007c2666826e73aaecdcbceae
Ves <veselynk@gmail.com>
2023-05-22 01:20:46 +0300

    Add Logo and Favicon to project

--- src/firebase/services/users.service.js
+++ src/firebase/services/users.service.js
@@ -1,14 +1,20 @@
 import { get, set, ref, query, equalTo, orderByChild } from 'firebase/database';
-import { db } from '../config/firebase-config';
+import { db } from '../firebase-config';
 
 export const getUserByHandle = (handle) => {
-    return get(ref(db, `users/${handle}`));
+  return get(ref(db, `users/${handle}`));
 };
 
 export const createUserHandle = (handle, uid, email) => {
-    return set(ref(db, `users/${handle}`), { handle, uid, email, createdOn: new Date(), likedPosts: {} });
+  return set(ref(db, `users/${handle}`), {
+    handle,
+    uid,
+    email,
+    createdOn: new Date(),
+    likedPosts: {},
+  });
 };
 
 export const getUserData = (uid) => {
-    return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+  return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
 };
