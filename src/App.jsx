08aafc2f0d9ddc0fca292c846fb2d6422ec85cc4
Ves <veselynk@gmail.com>
2023-05-21 21:55:48 +0300

    Create initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <ThemeProvider theme={theme}> */}
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+    {/* </ThemeProvider> */}
+  </React.StrictMode>
+);
