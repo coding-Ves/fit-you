bca790a643facd5e4566ff9ef5eb2868642af3f8
imadhm <imadhm98@gmail.com>
2023-05-21 22:45:42 +0300

    Add AuthContext implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './firebase/firebase-config';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import AuthContext from './contexts/AuthContext';
+import { getUserData } from './firebase/services/users.service';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [user, isLoading] = useAuthState(auth);
 
-  return <></>;
+    const [appState, setAppState] = useState({
+        user,
+        userData: null,
+    });
+
+    if (appState.user !== user) {
+        setAppState({ user });
+    }
+
+    useEffect(() => {
+        if (user === null) return;
+
+        getUserData(user.uid)
+            .then((snapshot) => {
+                if (!snapshot.exists()) {
+                    throw new Error('Something went wrong!');
+                }
+                setAppState({
+                    ...appState,
+                    userData: snapshot.val()[Object.keys(snapshot.val())[0]],
+                });
+            })
+            .catch((e) => alert(e.message));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user]);
+
+
+
+    return (
+        <>
+            <AuthContext.Provider value={{ ...appState, setContext: setAppState }} >
+            </AuthContext.Provider>
+        </>
+    );
 }
 
 export default App;
