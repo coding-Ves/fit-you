2d8f4d500547f0498983c46634519de83c4f0cf2
imadhm <imadhm98@gmail.com>
2023-05-22 18:28:02 +0300

    Add userRoles constant

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,9 @@
 export const exerciseDBUrl = 'https://exercisedb.p.rapidapi.com/exercises/';
 
 export const today = new Date().getTime();
+
+export const userRoles = {
+    user: 1,
+    admin: 2,
+    banned: 3,
+};
