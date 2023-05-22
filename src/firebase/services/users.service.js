6373bc25642d5728f622c5aa9b6401cd02ab80b0
imadhm <imadhm98@gmail.com>
2023-05-22 18:35:04 +0300

    Update createUserHandle function

--- src/firebase/services/users.service.js
+++ src/firebase/services/users.service.js
@@ -1,20 +1,30 @@
-import { get, set, ref, query, equalTo, orderByChild } from 'firebase/database';
+import {
+    get,
+    set,
+    ref,
+    query,
+    equalTo,
+    orderByChild,
+    update,
+} from 'firebase/database';
 import { db } from '../firebase-config';
+import { userRoles } from '../../common/constants';
 
 export const getUserByHandle = (handle) => {
-  return get(ref(db, `users/${handle}`));
-};
-
-export const createUserHandle = (handle, uid, email) => {
-  return set(ref(db, `users/${handle}`), {
-    handle,
-    uid,
-    email,
-    createdOn: new Date(),
-    likedPosts: {},
-  });
+    return get(ref(db, `users/${handle}`));
 };
 
 export const getUserData = (uid) => {
-  return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+    return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+};
+
+export const createUserHandle = (handle, uid, email, phoneNumber) => {
+    return set(ref(db, `users/${handle}`), {
+        handle,
+        uid,
+        email,
+        phoneNumber,
+        createdOn: Date.now(),
+        role: userRoles.user,
+    });
 };
