69c5646c6f1539f53602802d83db85e04d4364ff
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-05-23 13:40:07 +0300

    Add Dashboard View

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,8 +1,61 @@
+import { Grid, Paper } from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+// import Toolbar from '@mui/material/Toolbar';
+import { useState } from 'react';
+import Chart from '../../components/Dashboard/Chart/Chart';
+import RecentActivity from '../../components/Dashboard/RecentActivity/RecentActivity';
+import { mainListItems, secondaryListItems } from '../../components/SideBarDrawer/ListItems';
+import SideBarDrawer from '../../components/SideBarDrawer/SideBarDrawer';
+
 const Dashboard = () => {
+
     return (
-        <>
-            Dashboard
-        </>
+        <Box sx={{ display: 'flex' }}>
+            <SideBarDrawer
+                variant='permanent'
+                mainListItems={mainListItems}
+                secondaryListItems={secondaryListItems}
+            />
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[800],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto',
+                }}
+            >
+                {/* <Toolbar /> */}
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3}>
+                        {/* Chart */}
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    height: 240,
+                                }}
+                            >
+                                <Chart />
+                            </Paper>
+                        </Grid>
+
+                        {/* Recent Activity */}
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                <RecentActivity />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </Box>
     );
 };
 
