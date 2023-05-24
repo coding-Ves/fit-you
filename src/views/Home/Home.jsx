eb373ae5d2fbc0c84b78cef7e3b4043229d7858a
Ves <veselynk@gmail.com>
2023-05-25 01:37:40 +0300

    Add temp logout button and login status display

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,25 @@
+import { Button, TextField } from '@mui/material';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase/firebase-config';
+import { logoutUser } from '../../firebase/services/auth.service';
+
 const Home = () => {
+    const [user] = useAuthState(auth);
     return (
         <>
-            Home
+            <Button
+                onClick={() => {
+                    logoutUser();
+                }}
+            >
+                {' '}
+                Log out
+            </Button>
+            {user ? (
+                <TextField label='Logged in' />
+            ) : (
+                <TextField label='Not logged in' />
+            )}
         </>
     );
 };
