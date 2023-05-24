3defb6e71b072412ecb4f03a787e6319f397c9aa
Ves <veselynk@gmail.com>
2023-05-25 00:12:42 +0300

    Refactor form structure
    To  implement step form and reuse in my account.

---
+++ src/components/Forms/RegistrationForm.jsx
@@ -0,0 +1,210 @@
+import { useTheme } from '@emotion/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+import { useForm } from 'react-hook-form';
+import { formValidationSchema } from './../../services/formValidationSchema';
+import { useState } from 'react';
+import { IconButton, InputAdornment } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
+const RegistrationForm = () => {
+    const theme = useTheme();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    // Use React Hook Form with Yup for validation
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(formValidationSchema),
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component='main' maxWidth='xs'>
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component='h1' variant='h5'>
+                        Register
+                    </Typography>
+                    <Box
+                        component='form'
+                        noValidate
+                        onSubmit={handleSubmit(onSubmit)}
+                        sx={{ mt: 3 }}
+                    >
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete='username'
+                                    name='username'
+                                    required
+                                    fullWidth
+                                    id='username'
+                                    label='Username'
+                                    autoFocus
+                                    {...register('username')}
+                                    error={!!errors.username}
+                                    helperText={errors.username?.message}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id='email'
+                                    label='Email Address'
+                                    name='email'
+                                    autoComplete='email'
+                                    {...register('email')}
+                                    error={!!errors.email}
+                                    helperText={errors.email?.message}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name='password'
+                                    label='Password'
+                                    type={showPassword ? 'text' : 'password'}
+                                    id='password'
+                                    autoComplete='new-password'
+                                    {...register('password')}
+                                    error={!!errors.password}
+                                    helperText={errors.password?.message}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position='end'>
+                                                <IconButton
+                                                    onClick={() =>
+                                                        setShowPassword(
+                                                            !showPassword
+                                                        )
+                                                    }
+                                                    edge='end'
+                                                >
+                                                    {showPassword ? (
+                                                        <VisibilityOff />
+                                                    ) : (
+                                                        <Visibility />
+                                                    )}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name='confirmPassword'
+                                    label='Confirm Password'
+                                    id='confirmPassword'
+                                    autoComplete='new-password'
+                                    type={
+                                        showConfirmPassword
+                                            ? 'text'
+                                            : 'password'
+                                    }
+                                    {...register('confirmPassword')}
+                                    error={!!errors.confirmPassword}
+                                    helperText={errors.confirmPassword?.message}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position='end'>
+                                                <IconButton
+                                                    onClick={() =>
+                                                        setShowConfirmPassword(
+                                                            !showConfirmPassword
+                                                        )
+                                                    }
+                                                    edge='end'
+                                                >
+                                                    {showConfirmPassword ? (
+                                                        <VisibilityOff />
+                                                    ) : (
+                                                        <Visibility />
+                                                    )}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    name='phone'
+                                    label='Phone Number'
+                                    type='tel'
+                                    id='phone'
+                                    autoComplete='phone-number'
+                                    {...register('phoneNumber')}
+                                    error={!!errors.phoneNumber}
+                                    helperText={errors.phoneNumber?.message}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            value='allowExtraEmails'
+                                            color='primary'
+                                        />
+                                    }
+                                    label='I want to receive memes, marketing memes and memes via email.'
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type='submit'
+                            fullWidth
+                            variant='contained'
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent='flex-end'>
+                            <Grid item>
+                                <Link href='/login' variant='body2'>
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default RegistrationForm;
