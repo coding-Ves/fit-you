62b0e00e350dbfdfff40fbe5d0c7b4fd347d5813
Ves <veselynk@gmail.com>
2023-05-25 16:17:00 +0300

    Add NewGoalForm to My Goals view (temporary for visibility and testing)

--- src/views/MyGoals/MyGoals.jsx
+++ src/views/MyGoals/MyGoals.jsx
@@ -1,10 +1,12 @@
 import Box from '@mui/material/Box';
 import SideBarDrawer from '../../components/SideBarDrawer/SideBarDrawer';
+import NewGoalForm from '../../components/Forms/NewGoalForm';
 
 const MyGoals = () => {
     return (
         <Box sx={{ display: 'flex' }}>
             <SideBarDrawer />
+            <NewGoalForm />
         </Box>
     );
 };
