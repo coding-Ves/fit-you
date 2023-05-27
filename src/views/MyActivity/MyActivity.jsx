45bdb88f920cb3348170b6d7751f5355b179af3c
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-05-27 20:22:00 +0300

    Add MyActivity view

--- src/views/MyActivity/MyActivity.jsx
+++ src/views/MyActivity/MyActivity.jsx
@@ -1,4 +1,8 @@
+import { Grid, Paper } from '@mui/material';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import ActivitiesTable from '../../components/Activity/ActivitiesTable/ActivitiesTable';
+import AddActivityTable from '../../components/Activity/AddActivityTable/AddActivityTable';
 import SideBarDrawer from '../../components/SideBarDrawer/SideBarDrawer';
 
 const MyActivity = () => {
@@ -9,16 +13,30 @@ const MyActivity = () => {
             <Box
                 component="main"
                 sx={{
-                    backgroundColor: (theme) =>
-                        theme.palette.mode === 'light'
-                            ? theme.palette.grey[100]
-                            : theme.palette.grey[800],
-                    flexGrow: 1,
-                    height: '100vh',
-                    overflow: 'auto',
+                    flex: '1',
+                    display: 'flex',
                 }}
             >
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3} sx={{ display: 'flex', flexDirection: 'column'}}>
 
+                        {/* AddActivityTable */}
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper
+                                sx={{ p: 2, display: 'flex', flexDirection: 'column', height: '100%', }}>
+                                <AddActivityTable />
+                            </Paper>
+                        </Grid>
+
+                        {/* Activities Table */}
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                <ActivitiesTable />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+
+                </Container>
             </Box>
         </Box>
     );
