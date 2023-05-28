fff5e06cc2805d29ca80fada863b9855a55293f1
Ves <veselynk@gmail.com>
2023-05-28 13:56:07 +0300

    Add AboutSection & HowItWorks (temp)

--- src/components/Home/AboutSection/AboutSection.jsx
+++ src/components/Home/AboutSection/AboutSection.jsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import * as React from 'react';
 
-function AboutSection() {
-    return <div>AboutSection</div>;
-}
+import {
+    Box,
+    Button,
+    Typography,
+    Container,
+    Grid,
+    Card,
+    Avatar,
+} from '@mui/material';
+import { BatteryCharging20TwoTone } from '@mui/icons-material';
+
+const item = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minWidth: '200px;',
+    minHeight: '200px;',
+    p: 2,
+};
+
+const avatars = {
+    height: '100px',
+    width: '100px',
+};
+
+const AboutSection = () => {
+    return (
+        <Box>
+            <Typography variant='h4'>Meet the team</Typography>
+
+            <Box
+                component='section'
+                sx={{
+                    display: 'flex',
+                    overflow: 'hidden',
+                    justifyContent: 'center',
+                    // bgcolor: 'secondary.light',
+                }}
+            >
+                <Container
+                    sx={{
+                        mt: 5,
+                        mb: 5,
+                        display: 'flex',
+                        position: 'relative',
+                    }}
+                >
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={item}>
+                                <Avatar sx={avatars}>Bibi</Avatar>
+                                <Typography variant='h5' sx={{ my: 5 }}>
+                                    Person
+                                </Typography>
+                                <Typography variant='h6'>
+                                    Description
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={item}>
+                                <Avatar sx={avatars}>Imad</Avatar>
+                                <Typography variant='h5' sx={{ my: 5 }}>
+                                    Person
+                                </Typography>
+                                <Typography variant='h6'>
+                                    Description
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={item}>
+                                <Avatar sx={avatars}>Ves</Avatar>
+                                <Typography variant='h5' sx={{ my: 5 }}>
+                                    Person
+                                </Typography>
+                                <Typography variant='h6'>
+                                    Description
+                                </Typography>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </Box>
+    );
+};
 
 export default AboutSection;
--- src/components/Home/HowItWorks/HowItWorks.jsx
+++ src/components/Home/HowItWorks/HowItWorks.jsx
@@ -1,7 +1,114 @@
-import React from 'react';
+import * as React from 'react';
+import {
+    Box,
+    Grid,
+    Container,
+    Button,
+    Typography,
+    Card,
+    Avatar,
+} from '@mui/material';
+import { BatteryCharging20TwoTone } from '@mui/icons-material';
 
-function HowItWorks() {
-    return <div>HowItWorks</div>;
-}
+const item = {
+    minHeight: 200,
+    minWidth: 200,
+    px: 5,
+    p: 2,
+};
+const image = {
+    height: 100,
+    my: 4,
+};
+
+const HowItWorks = () => {
+    return (
+        <Box
+            component='section'
+            sx={{
+                display: 'flex',
+                bgcolor: 'secondary.light',
+                overflow: 'hidden',
+            }}
+        >
+            <Container
+                sx={{
+                    mt: 10,
+                    mb: 15,
+                    position: 'relative',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography
+                    variant='h4'
+                    marked='center'
+                    component='h2'
+                    sx={{ mb: 14 }}
+                >
+                    How it works
+                </Typography>
+                <div>
+                    <Grid container spacing={5}>
+                        <Grid item xs={12} md={4} sx={item}>
+                            <Card>
+                                <Box
+                                    component='img'
+                                    src='https://images.unsplash.com/photo-1571008887538-b36bb32f4571'
+                                    alt='suitcase'
+                                    sx={image}
+                                />
+                                <Box>1.</Box>
+
+                                <Typography variant='h5' align='center'>
+                                    Register and setup your account.
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4} sx={item}>
+                            <Card>
+                                <Box
+                                    component='img'
+                                    src='https://images.unsplash.com/photo-1571008887538-b36bb32f4571'
+                                    alt='suitcase'
+                                    sx={image}
+                                />
+                                <Box>2.</Box>
+
+                                <Typography variant='h5' align='center'>
+                                    Set up your goals.
+                                </Typography>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4} sx={item}>
+                            <Card>
+                                <Box
+                                    component='img'
+                                    src='https://images.unsplash.com/photo-1571008887538-b36bb32f4571'
+                                    alt='suitcase'
+                                    sx={image}
+                                />
+                                <Box>3.</Box>
+                                <Typography variant='h5' align='center'>
+                                    Log activities to track your progress.
+                                </Typography>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </div>
+                <Button
+                    color='secondary'
+                    size='large'
+                    variant='contained'
+                    component='a'
+                    sx={{ mt: 8 }}
+                >
+                    Get started
+                </Button>
+            </Container>
+        </Box>
+    );
+};
 
 export default HowItWorks;
