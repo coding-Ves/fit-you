d6539f027e6be4539f5d38580392fbf8abedae70
Ves <veselynk@gmail.com>
2023-05-28 17:35:29 +0300

    Fix Login form import, file location, split components

---
+++ src/components/Authentication/LogInForm/LogInForm.jsx
@@ -0,0 +1,167 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {
+    Alert,
+    Avatar,
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    Grid,
+    Link,
+    Snackbar,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
+
+import { useContext, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+
+import { auth } from '../../../firebase/firebase-config';
+import { loginUser } from '../../../firebase/services/auth.service';
+import AuthContext from './../../../contexts/AuthContext';
+
+const LoginForm = () => {
+    const { handleSubmit, register } = useForm();
+    const navigate = useNavigate();
+    // eslint-disable-next-line no-unused-vars
+    const [user] = useAuthState(auth);
+    const [isLoading, setIsLoading] = useState(false);
+    const { setContext } = useContext(AuthContext);
+
+    // Responsible for Snackbar and Alert - Showing error  and success messages
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState('');
+    const [snackbarSeverity, setSnackbarSeverity] = useState('success');
+
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
+
+    const onSubmit = (data) => {
+        setIsLoading(true);
+        loginUser(data.email, data.password)
+            .then((credential) => {
+                setContext({
+                    user: credential.user,
+                });
+            })
+            .then(() => {
+                setIsLoading('false');
+            })
+            .then(() => {
+                setSnackbarMessage(`Login successful!`);
+                setSnackbarSeverity('success');
+                setSnackbarOpen(true);
+            })
+            .catch((e) => {
+                setIsLoading(false);
+                setSnackbarMessage(e.code + ': ' + e.message);
+                setSnackbarSeverity('error');
+                setSnackbarOpen(true);
+            });
+    };
+
+    return (
+        <>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            >
+                <Alert
+                    onClose={handleSnackbarClose}
+                    severity={snackbarSeverity}
+                    sx={{ width: '100%' }}
+                >
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
+            <Box
+                sx={{
+                    my: 8,
+                    mx: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar
+                    sx={{
+                        m: 1,
+                        bgcolor: 'secondary.main',
+                    }}
+                >
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component='h1' variant='h5'>
+                    Login
+                </Typography>
+                <Box
+                    component='form'
+                    noValidate
+                    onSubmit={handleSubmit(onSubmit)}
+                    sx={{ mt: 1 }}
+                >
+                    <TextField
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='email'
+                        label='Email Address'
+                        name='email'
+                        autoComplete='email'
+                        autoFocus
+                        {...register('email')}
+                    />
+                    <TextField
+                        margin='normal'
+                        required
+                        fullWidth
+                        name='password'
+                        label='Password'
+                        type='password'
+                        id='password'
+                        autoComplete='current-password'
+                        {...register('password')}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value='remember' color='primary' />}
+                        label='Remember me'
+                    />
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        color='primary'
+                        fullWidth
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href='#' variant='body2'>
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link
+                                as={ReactRouterLink}
+                                to={`/register`}
+                                sx={{
+                                    color: 'primary.main',
+                                }}
+                            >
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </>
+    );
+};
+
+export default LoginForm;
--- src/views/LogIn/LogIn.jsx
+++ src/views/LogIn/LogIn.jsx
@@ -1,85 +1,14 @@
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useForm } from 'react-hook-form';
-import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
-import { LOGIN_IMAGE } from '../../common/constants.js';
-import AuthContext from '../../contexts/AuthContext';
-import { auth } from '../../firebase/firebase-config';
-import { useContext } from 'react';
-import { loginUser } from '../../firebase/services/auth.service';
-import { Snackbar, Alert } from '@mui/material';
+import { CssBaseline, Grid, Paper } from '@mui/material';
+import { LOGIN_IMAGE } from '../../common/constants';
+
+import LoginForm from '../../components/Authentication/LogInForm/LogInForm.jsx';
 
 const LogIn = () => {
-    const { handleSubmit, register } = useForm();
-    const navigate = useNavigate();
-    // eslint-disable-next-line no-unused-vars
-    const [user] = useAuthState(auth);
-    const [isLoading, setIsLoading] = useState(false);
-    const { setContext } = useContext(AuthContext);
-
-    // Responsible for Snackbar and Alert - Showing error  and success messages
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const [snackbarMessage, setSnackbarMessage] = useState('');
-    const [snackbarSeverity, setSnackbarSeverity] = useState('success');
-
-    const handleSnackbarClose = () => {
-        setSnackbarOpen(false);
-    };
-
-    const onSubmit = (data) => {
-        setIsLoading(true);
-        loginUser(data.email, data.password)
-            .then((credential) => {
-                setContext({
-                    user: credential.user,
-                });
-            })
-            .then(() => {
-                setIsLoading('false');
-            })
-            .then(() => {
-                setSnackbarMessage(`Login successful!`);
-                setSnackbarSeverity('success');
-                setSnackbarOpen(true);
-            })
-            .catch((e) => {
-                setIsLoading(false);
-                setSnackbarMessage(e.code + ': ' + e.message);
-                setSnackbarSeverity('error');
-                setSnackbarOpen(true);
-            });
-    };
-
     return (
         <>
             <Grid container component='main' sx={{ height: '100vh' }}>
                 <CssBaseline />
-                <Snackbar
-                    open={snackbarOpen}
-                    autoHideDuration={3000}
-                    onClose={handleSnackbarClose}
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                >
-                    <Alert
-                        onClose={handleSnackbarClose}
-                        severity={snackbarSeverity}
-                        sx={{ width: '100%' }}
-                    >
-                        {snackbarMessage}
-                    </Alert>
-                </Snackbar>
+
                 <Grid
                     item
                     xs={false}
@@ -101,92 +30,7 @@ const LogIn = () => {
                     elevation={6}
                     square
                 >
-                    <Box
-                        sx={{
-                            my: 8,
-                            mx: 4,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar
-                            sx={{
-                                m: 1,
-                                bgcolor: 'secondary.main',
-                            }}
-                        >
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component='h1' variant='h5'>
-                            Login
-                        </Typography>
-                        <Box
-                            component='form'
-                            noValidate
-                            onSubmit={handleSubmit(onSubmit)}
-                            sx={{ mt: 1 }}
-                        >
-                            <TextField
-                                margin='normal'
-                                required
-                                fullWidth
-                                id='email'
-                                label='Email Address'
-                                name='email'
-                                autoComplete='email'
-                                autoFocus
-                                {...register('email')}
-                            />
-                            <TextField
-                                margin='normal'
-                                required
-                                fullWidth
-                                name='password'
-                                label='Password'
-                                type='password'
-                                id='password'
-                                autoComplete='current-password'
-                                {...register('password')}
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value='remember'
-                                        color='primary'
-                                    />
-                                }
-                                label='Remember me'
-                            />
-                            <Button
-                                type='submit'
-                                variant='contained'
-                                color='primary'
-                                fullWidth
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href='#' variant='body2'>
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link
-                                        as={ReactRouterLink}
-                                        to={`/register`}
-                                        sx={{
-                                            color: 'primary.main',
-                                        }}
-                                    >
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
+                    <LoginForm />
                 </Grid>
             </Grid>
         </>
