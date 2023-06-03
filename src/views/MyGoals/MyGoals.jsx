c721e5b2ad2a0511bc0b042f66858472c6f52458
imadhm <imadhm98@gmail.com>
2023-06-04 01:47:16 +0300

    Add temporary goals to MyGoals view

--- src/views/MyGoals/MyGoals.jsx
+++ src/views/MyGoals/MyGoals.jsx
@@ -1,12 +1,18 @@
 import Box from '@mui/material/Box';
 import SideBarDrawer from '../../components/SideBarDrawer/SideBarDrawer';
 import NewGoalForm from './../../components/Goals/CreateGoal/NewGoalForm';
+import SingleActiveGoalCard from '../../components/Goals/SingleActiveGoalCard/SingleActiveGoalCard';
 
 const MyGoals = () => {
     return (
         <Box sx={{ display: 'flex' }}>
             <SideBarDrawer />
-            <NewGoalForm />
+            {/* <NewGoalForm /> */}
+            <Box sx={{ display: 'flex', p: 1 }}>
+                <SingleActiveGoalCard />
+                <SingleActiveGoalCard />
+                <SingleActiveGoalCard />
+            </Box>
         </Box>
     );
 };
