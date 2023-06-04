5b7566b6b093c8c6ab892ead28f3ef3c0b591c2b
Bilyana Mancheva <bilyana.mancheva1991@gmail.com>
2023-06-05 00:03:43 +0300

    Refactor SearchBar to use react hook form

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,10 +5,12 @@ import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ActivitiesContext } from '../../contexts/ActivitiesContext';
 import { searchFitness, searchSports } from './helpers/searchBarHelpers';
+import { set, useForm } from 'react-hook-form';
 
 const SearchBar = ({ category }) => {
 
-    const [search, setSearch] = useState('');
+    const { handleSubmit, register, reset } = useForm();
+    // const [search, setSearch] = useState('');
     const [placeholder, setPlaceholder] = useState('Search');
     const { setExercises, setCategory, setSports } = useContext(ActivitiesContext);
     const navigate = useNavigate();
@@ -18,38 +20,40 @@ const SearchBar = ({ category }) => {
             setPlaceholder('Search by name, target muscle, body part or equipment');
         } else if (category === 'sports') {
             setPlaceholder('Search by sports name or category');
+        } else if (category === 'cardio') {
+            setPlaceholder('Search by name');
         }
     }, [category]);
 
-    const handleSearch = () => {
-        if (search) {
+    const handleSearch = (data) => {
+        if (data && data.search) {
             //this handles special characters that could be in the search input
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
-            const encodedSearch = encodeURIComponent(search);
+            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+            const encodedSearch = encodeURIComponent(data.search);
             const searchQueryUrl = `/search/${category}?query=${encodedSearch}`;
 
             if (category === 'fitness') {
-                searchFitness(search)
+                searchFitness(data.search)
                     .then((result) => {
                         setExercises(result);
                         setCategory(category);
                         navigate(searchQueryUrl);
                     });
             } else if (category === 'sports') {
-                const result = searchSports(search);
+                const result = searchSports(data.search);
                 setSports(result);
                 setCategory(category);
                 navigate(searchQueryUrl);
             }
 
             window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-            setSearch('');
+            reset();
         }
     };
 
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
-            handleSearch();
+            handleSubmit(handleSearch)();
         }
     };
 
@@ -58,25 +62,28 @@ const SearchBar = ({ category }) => {
             <Typography fontWeight={700} sx={{ fontSize: '44px', '@media (max-width: 600px)': { fontSize: '30px' } }} mb="49px" textAlign="center">
                 {`Pick a ${category}-related activity!`}
             </Typography>
-            <Box position="relative" sx={{
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-            }}>
+            <Box position="relative"
+                component='form'
+                onSubmit={handleSubmit(handleSearch)}
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
                 <TextField
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value.toLowerCase())}
+                    {...register('search')}
                     onKeyDown={handleKeyDown}
                     placeholder={placeholder}
                     type="text"
                     sx={{
-                        width: '100%', // Updated width to take up the full width of the container
-                        maxWidth: '600px', // Added maxWidth to limit the width on larger screens
+                        width: '100%',
+                        maxWidth: '600px',
                     }}
                     InputProps={{
                         endAdornment: (
                             <InputAdornment position="end">
-                                <Button onClick={handleSearch}>
+                                <Button type='submit'>
                                     <SearchIcon />
                                 </Button>
                             </InputAdornment>
@@ -84,7 +91,7 @@ const SearchBar = ({ category }) => {
                     }}
                 />
             </Box>
-        </Stack>
+        </Stack >
     );
 };
 
