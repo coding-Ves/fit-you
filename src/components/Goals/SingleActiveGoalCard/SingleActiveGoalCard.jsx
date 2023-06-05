5809e290a08bfdf0df42d84f064a5df8cbcf3fc3
imadhm <imadhm98@gmail.com>
2023-06-06 00:45:00 +0300

    Update SingleActiveGoalCard to work with real data

--- src/components/Goals/SingleActiveGoalCard/SingleActiveGoalCard.jsx
+++ src/components/Goals/SingleActiveGoalCard/SingleActiveGoalCard.jsx
@@ -1,13 +1,9 @@
-import { Title } from '@mui/icons-material';
-import { Box, CircularProgress, LinearProgress, Paper, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import { Cell, Pie } from 'recharts';
-import { PieChart } from 'recharts';
+import { DAY_TO_MILLISECONDS } from '../../../common/constants';
 import ProgressPieChart from './ProgressPieChart/ProgressPieChart';
 
 // TODO - move the PieChart to a separate component
-
 
 const SingleActiveGoalCard = ({ goal }) => {
     return (
@@ -19,19 +15,31 @@ const SingleActiveGoalCard = ({ goal }) => {
                 }}
                 textAlign='center'
                 m={'10px 10px 0px 10px'}
-            >cry that many times</Typography>
-            <ProgressPieChart />
-            <Typography>
-                hello
+            >
+                {goal.goalName}
             </Typography>
-            <Box sx={{
-                display: 'flex', justifyContent: 'space-between', m: '0px 10px 5px 10px'
-            }}>
-                <Typography >
-                    Started on: <br /> 03/06/2020
+            <ProgressPieChart currentProgress={goal.goalProgress} goalTarget={goal.targetValue}/>
+            <Typography
+                fontWeight={500}
+                sx={{
+                    '@media (max-width: 600px)': { fontSize: '30px' },
+                }}
+                textAlign='center'
+            >
+                {goal.goalProgress} / {goal.targetValue} {goal.goalTargetType}
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    m: '0px 10px 5px 10px',
+                }}
+            >
+                <Typography>
+                    Started on: <br /> {new Date(goal.createdOn).toLocaleDateString('en-GB')}
                 </Typography>
-                <Typography >
-                    Target date: <br /> 03/06/2023
+                <Typography>
+                    Target date: <br /> {new Date(goal.createdOn + (goal.targetDate * DAY_TO_MILLISECONDS)).toLocaleDateString('en-GB')}
                 </Typography>
             </Box>
         </Box>
@@ -43,4 +51,3 @@ SingleActiveGoalCard.propTypes = {
 };
 
 export default SingleActiveGoalCard;
-
