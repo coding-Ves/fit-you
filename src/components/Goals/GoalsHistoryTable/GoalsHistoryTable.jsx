ad2cc6ccaa56ed80e12866c7c1a313865074ff49
imadhm <imadhm98@gmail.com>
2023-06-06 00:46:19 +0300

    Add base of GoalsHistoryTable component

---
+++ src/components/Goals/GoalsHistoryTable/GoalsHistoryTable.jsx
@@ -0,0 +1,11 @@
+// import PropTypes from 'prop-types';
+
+const GoalsHistoryTable = ({ goals }) => {
+    return <div>GoalsHistoryTable</div>;
+};
+
+GoalsHistoryTable.propTypes = {
+    // goals: PropTypes.array.isRequired,
+};
+
+export default GoalsHistoryTable;
