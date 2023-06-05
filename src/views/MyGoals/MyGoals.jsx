fbe74d94f2dbc970ca775b613a89a67a407bf08f
imadhm <imadhm98@gmail.com>
2023-06-06 00:25:09 +0300

    Update MyGoals view to follow website theme & add ActiveGoalsBoard to it.

--- src/views/MyGoals/MyGoals.jsx
+++ src/views/MyGoals/MyGoals.jsx
@@ -1,17 +1,45 @@
 import Box from '@mui/material/Box';
 import SideBarDrawer from '../../components/SideBarDrawer/SideBarDrawer';
 import NewGoalForm from './../../components/Goals/CreateGoal/NewGoalForm';
-import SingleActiveGoalCard from '../../components/Goals/SingleActiveGoalCard/SingleActiveGoalCard';
+import ActiveGoalsBoard from '../../components/Goals/ActiveGoalsBoard/ActiveGoalsBoard';
+import { Container, Grid, Paper } from '@mui/material';
+import { useContext } from 'react';
+import AuthContext from '../../contexts/AuthContext';
 
 const MyGoals = () => {
+    const { userData } = useContext(AuthContext);
+
     return (
         <Box sx={{ display: 'flex' }}>
             <SideBarDrawer />
-            {/* <NewGoalForm /> */}
-            <Box sx={{ display: 'flex', p: 1 }}>
-                <SingleActiveGoalCard />
-                <SingleActiveGoalCard />
-                <SingleActiveGoalCard />
+            <Box
+                component='main'
+                sx={{
+                    flex: '1',
+                    display: 'flex',
+                }}
+            >
+                <Container maxWidth='xl' sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3} sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                                <NewGoalForm />
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={12}>
+                            <Paper
+                                sx={{
+                                    p: 2,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    height: '100%',
+                                }}
+                            >
+                                <ActiveGoalsBoard username={userData?.username}/>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
             </Box>
         </Box>
     );
