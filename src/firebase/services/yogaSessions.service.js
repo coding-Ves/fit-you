b869bd6c20eda113d1d417cf9bf66f9b79c1bc18
Ves <veselynk@gmail.com>
2023-06-09 00:39:45 +0300

    Create yogaSessions.service.js

---
+++ src/firebase/services/yogaSessions.service.js
@@ -0,0 +1,59 @@
+import {
+    equalTo,
+    get,
+    orderByChild,
+    push,
+    query,
+    ref,
+    update,
+} from 'firebase/database';
+import { db } from '../firebase-config';
+
+export const addYogaSession = (username, poseName, durationInMinutes) => {
+    return push(ref(db, 'yogaSessions'), {
+        username,
+        poseName,
+        durationInMinutes,
+        createdOn: Date.now(),
+    }).then((result) => {
+        const id = result.key;
+        const updateYogaSession = {};
+        updateYogaSession[`/yogaSessions/${id}/id`] = id;
+        updateYogaSession[`/users/${username}/yogaSessions/${id}`] = true;
+        return update(ref(db), updateYogaSession).then(() => id);
+    });
+};
+
+export const getYogaSessionById = (id) => {
+    return get(ref(db, `yogaSessions/${id}`)).then((result) => {
+        if (!result.exists()) {
+            throw new Error(`Yoga session with id ${id} does not exist!`);
+        }
+        const yogaSessions = result.val();
+        yogaSessions.createdOn = new Date(
+            yogaSessions.createdOn
+        ).toLocaleString();
+        return yogaSessions;
+    });
+};
+
+export const getYogaSessionsByUsername = (username) => {
+    return get(
+        query(
+            ref(db, 'yogaSessions'),
+            orderByChild('username'),
+            equalTo(username)
+        )
+    ).then((result) => {
+        if (!result.exists()) return [];
+        const yogaSessions = Object.values(result.val());
+
+        yogaSessions.forEach((yogaSession) => {
+            yogaSession.createdOn = new Date(
+                yogaSession.createdOn
+            ).toLocaleString();
+        });
+
+        return yogaSessions;
+    });
+};
