ac4728f292d4769a7f478031ded1e2b0d8fcc931
Ves <veselynk@gmail.com>
2023-06-09 00:38:11 +0300

    Add yoga to SearchResultCard.jsx

--- src/components/SearchBar/SearchResultCard.jsx
+++ src/components/SearchBar/SearchResultCard.jsx
@@ -23,6 +23,8 @@ const SearchResultCard = ({ activity }) => {
             setImage(activity.gifUrl);
         } else if (category === 'sports' || category === 'cardio') {
             setImage(activity.imgUrl);
+        } else if (category === 'yoga') {
+            setImage(activity.url_png);
         }
     }, [activity.gifUrl, activity.imgUrl, category]);
 
@@ -30,16 +32,25 @@ const SearchResultCard = ({ activity }) => {
         setOpen(false);
     };
 
-    const shortenedActivityName =
-        activity.name.length > ACTIVITY_NAME_MAX_LENGTH
-            ? activity.name.substring(0, ACTIVITY_NAME_MAX_LENGTH) + '...'
-            : activity.name;
+    let shortenedActivityName = '';
+    if (activity.name) {
+        shortenedActivityName =
+            activity.name.length > ACTIVITY_NAME_MAX_LENGTH
+                ? activity.name.substring(0, ACTIVITY_NAME_MAX_LENGTH) + '...'
+                : activity.name;
+    } else if (activity.english_name) {
+        shortenedActivityName =
+            activity.english_name.length > ACTIVITY_NAME_MAX_LENGTH
+                ? activity.english_name.substring(0, ACTIVITY_NAME_MAX_LENGTH) +
+                  '...'
+                : activity.english_name;
+    }
 
     return (
         <Card variant='elevation' elevation={5} sx={{ maxWidth: 345 }}>
             <CardMedia
                 component='img'
-                alt={activity.name}
+                alt={activity.name || activity.english_name}
                 height='140'
                 image={image}
                 loading='lazy'
@@ -48,6 +59,26 @@ const SearchResultCard = ({ activity }) => {
                 <Typography gutterBottom variant='h5' component='div'>
                     {shortenedActivityName}
                 </Typography>
+
+                {/* extra fields for yoga cards  */}
+
+                {category === 'yoga' && (
+                    <Typography
+                        level='body3'
+                        sx={{ fontWeight: 'md', color: 'text.secondary' }}
+                    >
+                        Sanskrit Name: {activity.sanskrit_name}
+                    </Typography>
+                )}
+                {/* 
+                {category === 'yoga' && (
+                    <Typography
+                        level='body3'
+                        sx={{ fontWeight: 'md', color: 'text.secondary' }}
+                    >
+                        Translation: {activity.translation_name}
+                    </Typography>
+                )} */}
 
                 {/* extra fields for fitness cards */}
                 {category === 'fitness' && (
