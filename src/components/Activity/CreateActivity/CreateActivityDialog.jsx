e8767a288de215a07e03426399945776cd439d52
Ves <veselynk@gmail.com>
2023-06-09 00:37:13 +0300

    Add yoga as an option to createActivityDialog

--- src/components/Activity/CreateActivity/CreateActivityDialog.jsx
+++ src/components/Activity/CreateActivity/CreateActivityDialog.jsx
@@ -1,21 +1,47 @@
 /* eslint-disable react/prop-types */
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+} from '@mui/material';
 import { useParams } from 'react-router-dom';
 import CreateFitnessExerciseForm from './CreateFitnessExerciseForm';
 import CreateSportsOrCardioSessionForm from './CreateSportsOrCardioSessionForm';
 
 const CreateActivityDialog = ({ open, handleClose, activity }) => {
-
     const { category } = useParams();
 
     return (
         <>
             <Dialog open={open} onClose={handleClose} fullWidth={true}>
-                <DialogTitle>{activity.name.toUpperCase()}</DialogTitle>
+                {(category === 'fitness' ||
+                    category === 'sports' ||
+                    category === 'cardio') && (
+                    <DialogTitle>{activity.name.toUpperCase()}</DialogTitle>
+                )}
+                {category === 'yoga' && (
+                    <DialogTitle>
+                        {activity.english_name.toUpperCase()}
+                    </DialogTitle>
+                )}
                 <DialogContent>
-                    {category === 'fitness' && <CreateFitnessExerciseForm exercise={activity} category={category}/>}
-                    {(category === 'sports' || category === 'cardio') && 
-                    <CreateSportsOrCardioSessionForm activity={activity} category={category} handleClose={handleClose}/>}
+                    {category === 'fitness' && (
+                        <CreateFitnessExerciseForm
+                            exercise={activity}
+                            category={category}
+                        />
+                    )}
+                    {(category === 'sports' ||
+                        category === 'cardio' ||
+                        category === 'yoga') && (
+                        <CreateSportsOrCardioSessionForm
+                            activity={activity}
+                            category={category}
+                            handleClose={handleClose}
+                        />
+                    )}
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Close</Button>
